75c1d1e25cadc2c386f9c19caff0a9dee54bacee
Marcel Hieke <office@marcelhieke.com>
2023-02-13 12:13:10 +0100

    Add more Vue directives

--- src/Directives.php
+++ src/Directives.php
@@ -39,18 +39,18 @@ class Directives extends Register implements Registrable
                 $minify = new Minify\JS();
                 $minify->add("window.AppData.setData($data, $scope);");
 
-                return '<?php collectStack("vue", "' . $minify->minify() . '"); ?>';
+                return '<?php collectStack("vue", "'.$minify->minify().'"); ?>';
             },
 
             'vueMethod' => function (string $method, $scope = null) {
                 $minify = new Minify\JS();
                 $minify->add("window.AppData.setMethod($method, $scope);");
 
-                return '<?php collectStack("vue", "' . $minify->minify() . '"); ?>';
+                return '<?php collectStack("vue", "'.$minify->minify().'"); ?>';
             },
 
             'vueRelease' => function () { // TODO use window load to release VUE data
-                return '<?php echo "<script type=\"text/javascript\">window.addEventListener(\"load\", function() {" . releaseStack("vue") . "});</script>"; ?>';
+                return '<script type="text/javascript">window.addEventListener("AppDataInit", function() {<?= releaseStack("vue") ?>;});</script>';
             },
 
             'inlineScript' => function () {
@@ -77,11 +77,87 @@ class Directives extends Register implements Registrable
                         <?php collectPush($scope, $data); ?>
                     PHP;
                 }
+
                 return '';
             },
 
             'vuePayload' => function () {
                 return '<script type="application/json" id="app-data"><?= releasePush() ?></script>';
+            },
+
+            'attributes' => function ($attributes) {
+                if (preg_match('/\[{1}.*\]{1}/ms', $attributes, $matches)) {
+                    $cleaned = preg_replace("/\r|\n|\[|\]/", "", $matches[0]);
+                    $snippetArgs = array_map('trim', explode(",", $cleaned));
+                    $snippetArgs = rtrim(implode(",\n", $snippetArgs), ',');
+                    $attributes = preg_replace('/\[{1}.*\]{1}/ms', '['.$snippetArgs.']', $attributes);
+                }
+
+                return '<?php
+                    $attrs = '.$attributes.';
+                    foreach($attrs as $attr => $cond) {
+                        echo $cond ? " " . $attr : "";
+                    }
+                ?>';
+            },
+
+            'colorTheme' => function () {
+                return '<?= $site->theme()->or("light")->value(); ?>';
+            },
+
+            'autoTheme' => function () {
+                return '<?php if ($site->theme()->or("auto")->value() === "auto"): ?>
+                    <script>
+                        if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+                            document.documentElement.classList.add("dark");
+                            document.documentElement.classList.remove("light");
+                        }
+    
+                        if (localStorage.theme === "light" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: light)").matches)) {
+                            document.documentElement.classList.add("light");
+                            document.documentElement.classList.remove("dark");
+                        }
+    
+                        if (!("theme" in localStorage) && !window.matchMedia("(prefers-color-scheme: dark)").matches && !window.matchMedia("(prefers-color-scheme: light)").matches) {
+                            document.documentElement.classList.remove("light");
+                            document.documentElement.classList.remove("dark");
+                            document.documentElement.classList.add("auto");
+                        }
+                    </script>
+                <?php endif; ?>';
+            },
+
+            'themeColors' => function () {
+                $colors = site()->themecolors()->toStructure();
+                $cssVars = [];
+
+                foreach ($colors as $color) {
+                    $cssVars[] = '--theme-'.Str::slug($color->name()).': '.join(' ',
+                            array_filter($color->color()->toValues(), function ($key) {
+                                return array_key_exists($key, ['r' => 0, 'g' => 0, 'b' => 0]);
+                            }, ARRAY_FILTER_USE_KEY));
+                }
+
+                return '<style>:root,:before,:after{'.implode(';', $cssVars).(count($cssVars) ? ';' : '').'}</style>';
+            },
+
+            'headcode' => function () {
+                return '<?= $site->headcode()->or(null)->value(); ?>';
+            },
+
+            'footercode' => function () {
+                return '<?= $site->footercode()->or(null)->value(); ?>';
+            },
+
+            'externalLinksJs' => function ($path = null) {
+                if (!array_key_exists('nerdcel/kirby3-external-links', kirby()->plugins())) {
+                    return '';
+                }
+                if (!$path) {
+                    $path = 'media/plugins/nerdcel/kirby3-external-links/index.js';
+                }
+
+                return "<?php echo htmlentities(externalLinksJs(\"$path\", page())); ?>";
             }
         ];
     }
