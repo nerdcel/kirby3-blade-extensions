e1280318c64ea728cc75a30a42ac0826f5ab42c7
Marcel Hieke <office@marcelhieke.com>
2023-02-19 21:10:09 +0100

    Add className to vue component

--- src/Components/VueComponent.php
+++ src/Components/VueComponent.php
@@ -7,16 +7,18 @@ use Illuminate\View\Component;
 class VueComponent extends Component
 {
     public string $name;
+    public ?string $className;
 
-    public function __construct($name)
+    public function __construct($name, $class = null)
     {
         $this->name = $name;
+        $this->className = $class;
     }
 
     public function render()
     {
         return <<<'blade'
-            <div data-js-component="{{ $name }}">
+            <div data-js-component="{{ $name }}" @if($className)class="{{ $className }}"@endif>
                 {{ $slot }}
             </div>
         blade;
